f047430ae6a21dd853b6d4134665dade151f9ff3
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2022-08-15 12:49:16 +0200

    Upload page interface

---
+++ code/lib/pages/Page.cpp
@@ -0,0 +1,38 @@
+#ifndef PAGE_CPP
+#define PAGE_CPP
+
+#include "PageController.cpp"
+
+enum PageNum: int{
+    Stay,
+    HomePage,
+    SettingsPage1,
+    SettingsPage2,
+    EditEtvTimePage,
+    SettingsPage3,
+    ManualEtvPage,
+    SettingsPage4,
+    SettingsPage5,
+    SettingsPage6,
+    EditEtvDaysPage
+};
+
+class Page {
+private:
+
+public:
+    Page(PageController* controller) {
+        this->controller = controller;
+    }
+    virtual PageNum exec() { return Stay;}
+    virtual void show() {
+        controller->displayPrint("INTERFACCIA");
+    }
+
+protected:
+    PageController* controller;
+    bool redraw = true;
+
+};
+
+#endif
