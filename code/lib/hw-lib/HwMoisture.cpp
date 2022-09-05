2d563086ae5899a61d1a138ec7aac9b3f3178a5d
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2022-09-05 19:48:06 +0200

    Update HwMoisture.cpp

--- code/lib/hw-lib/HwMoisture.cpp
+++ code/lib/hw-lib/HwMoisture.cpp
@@ -12,7 +12,9 @@ public:
         _pin = pin;
     }
     uint8_t getMoisture() {
-        return analogRead(_pin);
+        uint8_t val = analogRead(_pin);
+        //return map(val, 0, 1023, 0, 100);
+        return 10;
     }
 protected:
 
