4f14c401ada1e9d969108d8ae7942f87276251b3
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-01-29 18:11:59 +0100

    forgot to add definitions to ClayDisplay

--- code/lib/clay-lib/ClayDisplay.cpp
+++ code/lib/clay-lib/ClayDisplay.cpp
@@ -8,46 +8,46 @@ void ClayDisplay::begin(ClayControlUnit* w) {
 	this->w = w;
 }
 
-void printSimpleText(const char text[]) {
+void ClayDisplay::printSimpleText(const char text[]) {
 	//TODO: add definition
 }
 
-void printData(const char text1[], int8_t data, const char text2[]) {
+void ClayDisplay::printData(const char text1[], int8_t data, const char text2[]) {
 	//TODO: add definition
 }
 
-void printData(const char text1[], int8_t data1, const char text2[], int8_t data2, const char text3[]) {
+void ClayDisplay::printData(const char text1[], int8_t data1, const char text2[], int8_t data2, const char text3[]) {
 	//TODO: add definition
 }
 
-void printIn(const char text[], int8_t x, int8_t y) {
+void ClayDisplay::printIn(const char text[], int8_t x, int8_t y) {
 	//TODO: add definition
 }
 
-void printIn(int8_t data, int8_t x, int8_t y) {
+void ClayDisplay::printIn(int8_t data, int8_t x, int8_t y) {
 	//TODO: add definition
 }
 
-void showClock(MyDateTime timeIn) {
+void ClayDisplay::showClock(MyDateTime timeIn) {
 	//TODO: add definition
 }
 
-void blinkAt(int8_t x, int8_t y) {
+void ClayDisplay::blinkAt(int8_t x, int8_t y) {
 	//TODO: add definition
 }
 
-void noBlink() {
+void ClayDisplay::noBlink() {
 	//TODO: add definition
 }
 
-void clockSym(bool state) {
+void ClayDisplay::clockSym(bool state) {
 	//TODO: add definition
 }
 
-void dropSym(bool state) {
+void ClayDisplay::dropSym(bool state) {
 	//TODO: add definition
 }
 
-void checkSym(bool state) {
+void ClayDisplay::checkSym(bool state) {
 	//TODO: add definition
 }
