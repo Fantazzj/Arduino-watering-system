b7aa0671d50ae05cd048afdb5b52e54afa5cbe99
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-01-29 16:08:51 +0100

    split main in three parts: variables, setup()/main() and loop()

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,7 +2,6 @@
 #include "../lib/core-lib/PageSelector.hpp"
 
 #if defined(QTDESKTOP)
-
 #	include "../lib/qt-lib/ControlUnit.h"
 #	include "../lib/qt-lib/QtClock.hpp"
 #	include "../lib/qt-lib/QtDisplay.hpp"
@@ -27,9 +26,56 @@ QtMoisture myMoisture;
 QtDebugger myDebugger;
 QtTimer myTimer;
 
+#elif defined(HWARDUINO)
+#	include "../lib/arduino-lib/HwClock.hpp"
+#	include "../lib/arduino-lib/HwDisplay.hpp"
+#	include "../lib/arduino-lib/HwKeypad.hpp"
+#	include "../lib/arduino-lib/HwMainSwitch.hpp"
+#	include "../lib/arduino-lib/HwMemory.hpp"
+#	include "../lib/arduino-lib/HwMoisture.hpp"
+#	include "../lib/arduino-lib/HwValveGroup.hpp"
+#	include "../lib/arduino-lib/HwDebugger.hpp"
+#	include "../lib/arduino-lib/HwTimer.hpp"
+
+//UnitDisplay
+#	define lcdAddress 0x27
+#	define lcdLength 16
+#	define lcdHeight 2
+
+//Etv
+const int8_t etvsPin[] = {6, 7, 2, 3, 4, 5, 8, 9, 10};
+#	define mainSwitchPin 11
+
+//Buttons
+#	define cancelPin A3
+#	define downPin A1
+#	define upPin A2
+#	define confirmPin A0
+
+//Sensors
+#	define humidityPin A7
+
+//Serial
+#	define baud 115200
+
+HwKeypad myKeypad(cancelPin, downPin, upPin, confirmPin);
+HwDisplay myDisplay(lcdAddress, lcdLength, lcdHeight);
+HwClock myClock;
+HwValveGroup myValveGroup(myClock, etvsPin);
+HwMainSwitch myMainSwitch(mainSwitchPin);
+HwMemory myMemory(VALVE_NUM);
+HwMoisture myMoisture(humidityPin);
+HwDebugger myDebugger;
+HwTimer myTimer;
+#elif defined(CLAYDESKTOP)
+#else
+#	error "No variable definition"
+#endif
+
 AutoCycle autoCycle(myClock, myValveGroup, VALVE_NUM, myMainSwitch, myMoisture, myTimer);
 PageSelector pageSelector(myKeypad, myDisplay, myClock, myValveGroup, myMainSwitch, myMemory, myTimer, autoCycle);
 
+#if defined(QTDESKTOP)
 void setup(ControlUnit* w) {
 	myDisplay.begin(w);
 	myClock.begin(w);
@@ -71,64 +117,18 @@ int main(int argc, char* argv[]) {
 
 	return exit;
 }
-
 #elif defined(HWARDUINO)
-
-#	include "../lib/arduino-lib/HwClock.hpp"
-#	include "../lib/arduino-lib/HwDisplay.hpp"
-#	include "../lib/arduino-lib/HwKeypad.hpp"
-#	include "../lib/arduino-lib/HwMainSwitch.hpp"
-#	include "../lib/arduino-lib/HwMemory.hpp"
-#	include "../lib/arduino-lib/HwMoisture.hpp"
-#	include "../lib/arduino-lib/HwValveGroup.hpp"
-#	include "../lib/arduino-lib/HwDebugger.hpp"
-#	include "../lib/arduino-lib/HwTimer.hpp"
-
-//UnitDisplay
-#	define lcdAddress 0x27
-#	define lcdLength 16
-#	define lcdHeight 2
-
-//Etv
-const int8_t etvsPin[] = {6, 7, 2, 3, 4, 5, 8, 9, 10};
-#	define mainSwitchPin 11
-
-//Buttons
-#	define cancelPin A3
-#	define downPin A1
-#	define upPin A2
-#	define confirmPin A0
-
-//Sensors
-#	define humidityPin A7
-
-//Serial
-#	define baud 115200
-
-HwKeypad myKeypad(cancelPin, downPin, upPin, confirmPin);
-HwDisplay myDisplay(lcdAddress, lcdLength, lcdHeight);
-HwClock myClock;
-HwValveGroup myEtv(myClock, etvsPin);
-HwMainSwitch myMainSwitch(mainSwitchPin);
-HwMemory myMemory(VALVE_NUM);
-HwMoisture myMoisture(humidityPin);
-HwDebugger myDebugger;
-HwTimer myTimer;
-
-AutoCycle autoCycle(myClock, myEtv, VALVE_NUM, myMainSwitch, myMoisture, myTimer);
-PageSelector pageSelector(myKeypad, myDisplay, myClock, myEtv, myMainSwitch, myMemory, myTimer, autoCycle);
-
 void setup() {
 	myDisplay.begin();
 	myClock.begin();
 	myMainSwitch.begin();
 	myMemory.begin();
-	myEtv.begin();
+	myValveGroup.begin();
 	myDebugger.begin(115200);
 
 	for(int8_t i = 0; i < VALVE_NUM; i++) {
-		myEtv.setMinOn(i, myMemory.readEtvMinOn(i));
-		myEtv.setDays(i, myMemory.readEtvDays(i));
+		myValveGroup.setMinOn(i, myMemory.readEtvMinOn(i));
+		myValveGroup.setDays(i, myMemory.readEtvDays(i));
 	}
 
 	MyTime tStart = myMemory.readStartTime();
@@ -136,10 +136,55 @@ void setup() {
 	autoCycle.begin(tStart);
 	pageSelector.begin();
 }
+#elif defined(CLAYDESKTOP)
+
+#	include "../lib/clay-lib/ClayControlUnit.hpp"
+
+/*
+void setup(ControlUnit* w) {
+	myDisplay.begin(w);
+	myClock.begin(w);
+	myMainSwitch.begin(w);
+	myMemory.begin();
+	myMoisture.begin(w);
+	myValveGroup.begin(w);
+
+	for(int8_t i = 0; i < VALVE_NUM; i++) {
+		myValveGroup.setDays(i, myMemory.readEtvDays(i));
+		myValveGroup.setMinOn(i, myMemory.readEtvMinOn(i));
+	}
+
+	MyTime tStart = myMemory.readStartTime();
+	autoCycle.begin(tStart);
+
+	pageSelector.begin();
+}
+ */
+
+void loop();
+
+int main(int argc, char* argv[]) {
+	ClayControlUnit w;
+
+	//w.setKeypad(&myKeypad);
+	//setup(&w);
+
+	QThread* thread = QThread::create([] {
+		for(;;) {
+			loop();
+			QThread::usleep(10);
+		}
+	});
+	thread->start();
+
+	w.show();
+	int exit = a.exec();
+
+	return exit;
+}
 
 #else
-#	error Missing setup() definition
-
+#	error "No setup() definition"
 #endif
 
 void loop() {
