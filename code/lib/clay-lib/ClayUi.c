ac5021eac6aa8d7cadc90712a379e16d4ec14208
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-01-30 11:43:27 +0100

    format update

--- code/lib/clay-lib/ClayUi.c
+++ code/lib/clay-lib/ClayUi.c
@@ -13,31 +13,29 @@ void init() {
 	Clay_Raylib_Initialize(800, 600, "ControlUnit", FLAG_WINDOW_RESIZABLE);
 
 	uint64_t clayRequiredMemory = Clay_MinMemorySize();
-	Clay_Arena clayMemory = (Clay_Arena){
-			.capacity = clayRequiredMemory,
-			.memory = (char*) malloc(clayRequiredMemory)};
+	Clay_Arena clayMemory = (Clay_Arena){.capacity = clayRequiredMemory, .memory = (char*) malloc(clayRequiredMemory)};
 
-	Clay_Initialize(clayMemory,
-					(Clay_Dimensions){
-							.width = (float) GetScreenWidth(),
-							.height = (float) GetScreenHeight()},
-					(Clay_ErrorHandler){HandleClayErrors});
+	Clay_Initialize(
+			clayMemory,
+			(Clay_Dimensions){.width = (float) GetScreenWidth(), .height = (float) GetScreenHeight()},
+			(Clay_ErrorHandler){HandleClayErrors}
+	);
 }
 
 void show() {
 	while(!WindowShouldClose()) {
 		Clay_BeginLayout();
 
-		Clay_SetLayoutDimensions((Clay_Dimensions){
-				.width = (float) GetScreenWidth(),
-				.height = (float) GetScreenHeight()});
+		Clay_SetLayoutDimensions((Clay_Dimensions){.width = (float) GetScreenWidth(), .height = (float) GetScreenHeight()});
 
-		CLAY(
-				CLAY_RECTANGLE({.color = {0, 0, 255, 255}}),
-				CLAY_LAYOUT({.sizing = {
-									 .height = CLAY_SIZING_GROW(),
-									 .width = CLAY_SIZING_GROW(),
-							 }}));
+		CLAY(CLAY_RECTANGLE({.color = {0, 0, 255, 255}}),
+			 CLAY_LAYOUT(
+					 {.sizing =
+							  {
+									  .height = CLAY_SIZING_GROW(),
+									  .width = CLAY_SIZING_GROW(),
+							  }}
+			 ));
 
 		Clay_RenderCommandArray renderCommands = Clay_EndLayout();
 
