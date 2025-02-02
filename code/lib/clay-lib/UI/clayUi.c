c2e5a58e9986eef3f6e178de37cf6409d730c305
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-02-02 15:45:09 +0100

    basics for buttons

--- code/lib/clay-lib/UI/clayUi.c
+++ code/lib/clay-lib/UI/clayUi.c
@@ -10,6 +10,14 @@
 #define D_HEIGHT 2
 #define D_LENGTH 16
 
+#define B_NUM 4
+
+const char buttonNames[B_NUM][8] = {
+		"Cancel",
+		"Down",
+		"Up",
+		"Confirm",
+};
 
 const char text[D_HEIGHT][D_LENGTH] = {
 		"simple text row1",
@@ -117,10 +125,60 @@ void createDisplay(int8_t rows, int8_t columns) {
 	}
 }
 
+void createButton(int8_t i) {
+	Clay_String buttonName = {
+			.chars = buttonNames[i],
+			.length = 2,
+	};
+
+	Clay_TextElementConfig buttonText = {
+			.fontId = 0,
+			.fontSize = 48,
+			.textColor = {255, 255, 255, 255},
+	};
+
+	CLAY(CLAY_RECTANGLE({.color = {255, 0, 0, 255}}),
+		 CLAY_LAYOUT({
+				 .sizing = {
+						 .width = CLAY_SIZING_GROW(),
+						 .height = CLAY_SIZING_FIT(),
+				 },
+				 .childAlignment = {CLAY_ALIGN_X_CENTER, CLAY_ALIGN_Y_CENTER},
+		 }),
+		 CHILD) {
+		CLAY_TEXT(buttonName, CLAY_TEXT_CONFIG(buttonText));
+	}
+}
+
+void createButtons() {
+	CLAY(CLAY_ID("Buttons"),
+		 CLAY_LAYOUT({
+				 .layoutDirection = CLAY_LEFT_TO_RIGHT,
+				 .padding = {5, 5, 5, 5},
+				 .childGap = 5,
+				 .childAlignment = {CLAY_ALIGN_X_CENTER, CLAY_ALIGN_Y_CENTER},
+				 .sizing = {
+						 .width = CLAY_SIZING_GROW(),
+						 .height = CLAY_SIZING_FIT(),
+				 },
+		 }),
+		 CHILD) {
+		for(int8_t i = 0; i < B_NUM; i++)
+			createButton(i);
+	}
+}
+
 void createControlUnit() {
-	createDisplay(D_HEIGHT, D_LENGTH);
-	//createButtons();
-	//createEtvs();
+	CLAY(CLAY_ID("ControlUnit"),
+		 CLAY_LAYOUT({
+				 .layoutDirection = CLAY_TOP_TO_BOTTOM,
+				 .childGap = 5,
+		 }),
+		 CHILD) {
+		createDisplay(D_HEIGHT, D_LENGTH);
+		createButtons();
+		//createEtvs();
+	}
 }
 
 void createDebugger() {
