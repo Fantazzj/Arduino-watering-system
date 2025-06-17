26a5b654192f04e4eaa828120e2f09b68b5a13d6
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-06-17 23:22:28 +0200

    probably there was some memory leaked

--- code/lib/clay-lib/ClayClock.cpp
+++ code/lib/clay-lib/ClayClock.cpp
@@ -16,9 +16,13 @@ MyDateTime ClayClock::getDateTime() {
 	const auto now_time = system_clock::to_time_t(now);
 	const auto time = localtime(&now_time);
 
-	return MyDateTime(
+	const auto dateTime = MyDateTime(
 			MyTime(time->tm_hour, time->tm_min, time->tm_sec),
 			MyDate(time->tm_wday + 1, time->tm_mday, time->tm_mon + 1, time->tm_year + 1900));
+
+	delete time;
+
+	return dateTime;
 }
 
 void ClayClock::setDateTime(const MyDateTime time) {
