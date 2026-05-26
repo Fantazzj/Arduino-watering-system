ca6e491b2d9bd7ed520434cfb8ab2f0746047cc7
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2026-05-26 19:17:13 +0200

    different files for each main

---
+++ code/src/desktopMain.cpp
@@ -0,0 +1,82 @@
+#include "../lib/core/AutoCycle.hpp"
+#include "../lib/core/PageSelector.hpp"
+#include "../lib/desktop-clay/ClayClock.hpp"
+#include "../lib/desktop-clay/ClayControlUnit.hpp"
+#include "../lib/desktop-clay/ClayDebugger.hpp"
+#include "../lib/desktop-clay/ClayDisplay.hpp"
+#include "../lib/desktop-clay/ClayKeypad.hpp"
+#include "../lib/desktop-clay/ClayMainSwitch.hpp"
+#include "../lib/desktop-clay/ClayMemory.hpp"
+#include "../lib/desktop-clay/ClayMoisture.hpp"
+#include "../lib/desktop-clay/ClayTimer.hpp"
+#include "../lib/desktop-clay/ClayValveGroup.hpp"
+
+#include <chrono>
+#include <thread>
+
+using std::thread;
+using std::chrono::microseconds;
+using std::this_thread::sleep_for;
+
+ClayKeypad myKeypad;
+ClayDisplay myDisplay;
+ClayClock myClock;
+ClayValveGroup myValveGroup(myClock);
+ClayMainSwitch myMainSwitch;
+ClayMemory myMemory("./memory.bin");
+ClayMoisture myMoisture;
+ClayDebugger myDebugger;
+ClayTimer myTimer;
+
+AutoCycle autoCycle(myClock, myValveGroup, myMainSwitch, myMoisture, myTimer, myDebugger);
+PageSelector pageSelector(myKeypad, myDisplay, myClock, myValveGroup, myMainSwitch, myMemory, myTimer, myDebugger, autoCycle);
+
+void setup(ClayControlUnit* w) {
+	myDisplay.begin(w);
+	myClock.begin(w);
+	myKeypad.begin(w);
+	myMainSwitch.begin(w);
+	myMemory.begin();
+	myMoisture.begin(w);
+	myValveGroup.begin(w);
+	myDebugger.begin(w);
+
+	for(int8_t i = 0; i < VALVE_NUM; i++) {
+		myValveGroup.setDays(i, myMemory.readEtvDays(i));
+		myValveGroup.setMinOn(i, myMemory.readEtvMinOn(i));
+	}
+
+	MyTime tStart = myMemory.readStartTime();
+	autoCycle.begin(tStart);
+
+	pageSelector.begin();
+}
+
+void loop() {
+	pageSelector.show();
+
+	pageSelector.exec();
+
+	autoCycle.exec();
+}
+
+int main(int argc, char* argv[]) {
+	ClayControlUnit w;
+	bool finished = false;
+
+	setup(&w);
+
+	bool* pfinished = &finished;
+	thread thread([pfinished] {
+		while(!*pfinished) {
+			loop();
+			sleep_for(microseconds(10));
+		}
+	});
+
+	w.show();
+	finished = true;
+	thread.join();
+
+	return 0;
+}
